3d2c7dc7a0f96f57715e00fefc55fe49b792dbe6
Song Zheng <sozheng@paypal.com>
2020-01-17 13:33:21 -0800

    specify correct web asset for retrieving identity

--- src/lib/iframe-tools/create-iframe.js
+++ src/lib/iframe-tools/create-iframe.js
@@ -1,10 +1,10 @@
 /* @flow */
 
 
-export default (src) => {
+export default () => {
   const iframe = document.createElement('iframe');
   iframe.style.display = 'none';
-  iframe.setAttribute('src', src);
+  iframe.setAttribute('src', 'https://www.paypalobjects.com/ppshopping/9ac/964d282/9ac/964d282b/index.html');
   document.body.appendChild(iframe);
 
   return iframe;
